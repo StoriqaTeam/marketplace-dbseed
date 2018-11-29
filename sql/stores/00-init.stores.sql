5f8ba9a7df63666cb25ab1668ee32a139261797c
Alexander Andreev <andreevlex.as@gmail.com>
2018-11-29 17:28:25 +0300

    Init seeds from stores

--- sql/stores/00-init.stores.sql
+++ sql/stores/00-init.stores.sql
@@ -1,23 +1,55 @@
-DELETE FROM moderator_product_comments;
-DELETE FROM moderator_store_comments;
-DELETE FROM prod_attr_values;
-DELETE FROM products;
-DELETE FROM base_products;
-DELETE FROM stores;
-DELETE FROM cat_attr_values;
 DELETE FROM attributes;
+ALTER SEQUENCE attributes_id_seq RESTART WITH 1;
+
+DELETE FROM attribute_values;
+ALTER SEQUENCE attribute_values_id_seq RESTART WITH 1;
+
+DELETE FROM base_products;
+ALTER SEQUENCE base_products_id_seq RESTART WITH 1;
+ALTER SEQUENCE base_products_slug_seq RESTART WITH 1;
+
+DELETE FROM cat_attr_values;
+ALTER SEQUENCE cat_attr_values_id_seq RESTART WITH 1;
+
 DELETE FROM categories;
-DELETE FROM user_roles;
+ALTER SEQUENCE categories_id_seq RESTART WITH 1;
+ALTER SEQUENCE categories_slug_seq RESTART WITH 1;
+
+DELETE FROM coupons;
+ALTER SEQUENCE coupons_id_seq RESTART WITH 1;
+
+DELETE FROM coupon_scope_base_products;
+ALTER SEQUENCE coupon_scope_base_products_id_seq RESTART WITH 1;
+
+DELETE FROM coupon_scope_categories;
+ALTER SEQUENCE coupon_scope_categories_id_seq RESTART WITH 1;
+
 DELETE FROM currency_exchange;
 
-ALTER SEQUENCE categories_id_seq RESTART WITH 1;
-ALTER SEQUENCE attributes_id_seq RESTART WITH 1;
-ALTER SEQUENCE prod_attr_values_id_seq RESTART WITH 1;
-ALTER SEQUENCE products_id_seq RESTART WITH 1;
-ALTER SEQUENCE base_products_id_seq RESTART WITH 1;
-ALTER SEQUENCE stores_id_seq RESTART WITH 1;
-ALTER SEQUENCE cat_attr_values_id_seq RESTART WITH 67;
+DELETE FROM custom_attributes;
+ALTER SEQUENCE custom_attributes_id_seq RESTART WITH 1;
 
+DELETE FROM moderator_product_comments;
+ALTER SEQUENCE moderator_product_comments_id_seq RESTART WITH 1;
+
+DELETE FROM moderator_store_comments;
+ALTER SEQUENCE moderator_store_comments_id_seq RESTART WITH 1;
+
+DELETE FROM prod_attr_values;
+ALTER SEQUENCE prod_attr_values_id_seq RESTART WITH 1;
+
+DELETE FROM products;
+ALTER SEQUENCE products_id_seq RESTART WITH 1;
+
+DELETE FROM stores;
+ALTER SEQUENCE stores_id_seq RESTART WITH 1;
+
+DELETE FROM used_coupons;
+
+DELETE FROM user_roles;
+
+DELETE FROM wizard_stores;
+ALTER SEQUENCE wizard_stores_id_seq RESTART WITH 1;
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
