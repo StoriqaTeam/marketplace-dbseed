a26c585db555cb3b991b2ad375af06c0aee0e5b0
busarovalex <busarovalex@yandex.ru>
2018-12-20 15:54:08 +0300

    Update search query

--- elastic-sync/store-search-test.sh
+++ elastic-sync/store-search-test.sh
@@ -6,7 +6,7 @@ declare -a arr=("foxwood_rus" "foxwood" "xwood" "foxwod" "fo" "fox" "fa" "fax" "
 for i in "${arr[@]}"
 do
    echo "$i"
-   query='{"from":0,"query":{"bool":{"filter":[{"term":{"status":"published"}}],"must":{"nested":{"path":"name","query":{"fuzzy":{"name.text":{"value":"'$i'"}}}}}}}}'
+   query='{"from":0,"query":{"bool":{"filter":[{"term":{"status":"published"}}],"must":{"nested":{"path":"name","query":{"fuzzy":{"name.text":{"value":"'$i'","boost":1.0,"fuzziness":2,"prefix_length":0}}}}}}}}'
    curl -s -H "Content-Type:application/json" -X POST http://localhost:32776/stores/_search -d $query | jq -c -r '.hits.hits'
    printf '\n'
 done
