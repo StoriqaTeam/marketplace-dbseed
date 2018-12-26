619e0e8a6f72a4b5bdacd53cec9dc67bab6d64e8
busarovalex <busarovalex@yandex.ru>
2018-12-26 16:55:04 +0300

    remove multimatch

--- elastic-sync/store-search-test.sh
+++ elastic-sync/store-search-test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # {"from":0,"query":{"bool":{"filter":[{"term":{"status":"published"}}],"must":{"nested":{"path":"name","query":{"match":{"name.text":"INPUT"}}}}}}}
 
-declare -a arr=("foxwood_rus" "foxwood" "xwood" "foxwod" "faxwood" "xwood" "faxwod")
+declare -a arr=("foxwood_rus" "foxwood" "xwood" "foxwod" "faxwood" "xwood" "faxwod" "foxwood_ru")
 
 for i in "${arr[@]}"
 do
    echo "$i"
-   query='{"from":0,"query":{"bool":{"filter":[{"term":{"status":"published"}}],"must":{"nested":{"path":"name","query":{"multi_match":{"query":"'$i'","fuzziness":"AUTO","fields":["name.text.edge_ngram_search","name.text.ngram_search"],"type":"most_fields"}}}}}}}'
+   query='{"from":0,"query":{"bool":{"filter":[{"term":{"status":"published"}}],"must":{"nested":{"path":"name","query":{"match":{"name.text":{"query":"'$i'","fuzziness":"AUTO"}}}}}}}}'
    curl -s -H "Content-Type:application/json" -X POST http://localhost:32813/stores/_search -d $query | jq -c -r '.hits.hits'
    printf '\n'
 done
